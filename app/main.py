cad99440a0466fe57dc3570a56c7470655fd3a1d
VitaliiDats1 <vitalii.8dats@gmail.com>
2024-05-22 12:41:07 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class:
+                 int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -21,22 +23,27 @@ class CarWashStation:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                income += self.calculate_washing_price(car)
-                self.wash_single_car(car)
+                price = self.calculate_washing_price(car)
+                if price is not None:
+                    income += price
+                    self.wash_single_car(car)
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class
-                     * (self.clean_power - car.clean_mark)
-                     * self.average_rating
-                     / self.distance_from_city_center, 1)
+        if car.clean_mark >= self.clean_power:
+            return 0  # Car doesn't need washing
+        price = (car.comfort_class * (self.clean_power - car.clean_mark)
+                 * self.average_rating / self.distance_from_city_center)
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-        self.average_rating = round((self.average_rating
-                                     * self.count_of_ratings + rate)
+        total_rating = (self.average_rating
+                        * self.count_of_ratings)
+        total_rating = total_rating + rate
+        self.average_rating = round(total_rating
                                     / (self.count_of_ratings + 1), 1)
-        self.count_of_ratings += 1
+        self.count_of_ratings = self.count_of_ratings + 1
