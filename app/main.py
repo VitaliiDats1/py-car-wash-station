702d6acf93634a81fc8bca5c49a26619b78ce989
VitaliiDats1 <vitalii.8dats@gmail.com>
2024-05-22 21:55:53 +0300

    'Solution

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list) -> int | float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -30,8 +30,6 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        if car.clean_mark >= self.clean_power:
-            return 0  # Car doesn't need washing
         price = (car.comfort_class * (self.clean_power - car.clean_mark)
                  * self.average_rating / self.distance_from_city_center)
         return round(price, 1)
